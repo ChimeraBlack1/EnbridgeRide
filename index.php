a3a9c4697aaac2f967c9f716327e2a6b607e8db5
chimerablack <christopherparke@hotmail.com>
2016-11-17 13:04:16 -0500

    initial commit for enbridgeRide site

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    
+      <header class='header'>
+          <nav>              
+              <ul class="nav nav-pills">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Donate</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Join the Team</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Team Information</a>
+                  </li>
+                  <li class="nav-item-right">
+                    <a class="nav-link" href="http://conquercancer.ca">Enbridge Ride to Conquer Cancer</a>
+                  </li>
+              </ul>
+          </nav>
+      </header>
+      <h1>Team Lakeshore Yacht Club</h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 centered">
+                Introduction about event and team Mission Statement.
+            </div>
+            <div class="col-sm-6 centered">
+                <img src="Hlogo.jpg">
+            </div>
+        </div>
+    </div>
+
+      <section id='donate'>
+          <button type="button">Donate</button>
+      </section>
+      <section id='join'>
+          <button type="button">Join the Team</button>
+      </section>
+      <section id='info'>
+          <button type="button">Team info</button>
+          <ul>
+            <li>Team Updates &amp; Announcements</li>
+            <li>Enbridge Deals</li>
+            <li>Spinning Classes</li>
+            <li>Playlists</li>
+            <li>Articles</li>
+          </ul>
+      </section>
+      <footer class='footer'>
+        <section class='footer-section'>
+            <a href='#'>Donate</a>
+          </section>
+        <section class='footer-section'>
+            <a href='#'>Contact for more</a>
+          </section>
+        <section class='footer-section'>
+             <a href='#'>Enbridge Ride</a></section>
+      </footer>     
+      
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+  </body>
+</html>
